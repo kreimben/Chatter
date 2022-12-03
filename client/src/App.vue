d57b7f775aec1a33c289aacf4f6dfce3ab60869e
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-04 00:18:39 +0900

    Vue settings

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <Auth />
+      <MainPage />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import MainPage from '@/components/MainPage.vue'
+  import Auth from '@/components/Menu.vue'
+
+
+</script>
